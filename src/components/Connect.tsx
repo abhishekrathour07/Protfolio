2666f3a76fe414420208ca9880664ea19f9b73b2
abhishekrathour07 <singhrathourabhishek7@gmail.com>
2024-11-26 19:29:28 +0530

    fixed the responsive

--- src/components/Connect.tsx
+++ src/components/Connect.tsx
@@ -7,13 +7,15 @@ import { toast } from 'sonner';
 
 const Connect: React.FC = () => {
     const LoginSchema = yup.object().shape({
-        name: yup.string().min(8, 'Minimum character should be 8').required('Name is required'),
+        name: yup.string().min(5, 'Minimum character should be 8').required('Name is required'),
+        email: yup.string().email().required('Name is required'),
         content: yup.string().min(12, 'Message must be at least 12 characters').required('Message is required'),
     });
 
     const form = useForm({
         defaultValues: {
             name: "",
+            email: "",
             content: "",
         },
         resolver: yupResolver(LoginSchema),
@@ -29,6 +31,7 @@ const Connect: React.FC = () => {
             body: JSON.stringify({
                 access_key: "4ca157b1-ff8b-47ad-a552-040e75dc694b",
                 name: data.name,
+                email: data.email,
                 content: data.content,
             }),
         });
@@ -42,9 +45,9 @@ const Connect: React.FC = () => {
     };
 
     return (
-        <div className="h-[60vh] md:h-[50vh] flex justify-center mt-12 mx-6">
-            <div className="bg-[#9694FF] md:w-[70vw] md:h-[40vh] lg:w-[60vw] items-center rounded-lg flex flex-col md:flex-row">
-                <div className="bg-[#231d1d] rounded-lg md:rounded-tr-3xl rounded-br-[4rem] h-44 md:h-full md:w-[40%] flex flex-col justify-center gap-2 p-7">
+        <div className="h-[70vh] md:h-[50vh] flex justify-center mt-12 mx-6 ">
+            <div className="bg-[#9694FF] md:w-[70vw] md:h-[45vh] lg:w-[60vw] items-center rounded-lg flex flex-col md:flex-row">
+                <div className="bg-[#231d1d] rounded-lg md:rounded-tr-3xl rounded-br-[4rem] h-44 md:h-full md:w-[40%] flex flex-col justify-center gap-2 p-7 text-white">
                     <h2 className="space-x-1 text-4xl">Contact</h2>
                     <p className="h-2 w-12 bg-slate-600 rounded-2xl mr-16"></p>
                     <p className="text-lg">Any questions or remarks? Just write me a message :)</p>
@@ -69,6 +72,23 @@ const Connect: React.FC = () => {
                             )}
                         />
                         <FormField
+                            name="email"
+                            control={form.control}
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormControl className="mt-4">
+                                        <input
+                                            {...field}
+                                            className="mt-6 w-full px-3 py-3 border rounded-2xl text-sm focus:outline-none bg-[#f1f0e8be] text-black"
+                                            placeholder="Enter your email"
+                                            type="email"
+                                        />
+                                    </FormControl>
+                                    <FormMessage className="text-xs text-red-600 mt-1" />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
                             name="content"
                             control={form.control}
                             render={({ field }) => (
@@ -86,7 +106,7 @@ const Connect: React.FC = () => {
                         />
                         <button
                             type="button"
-                            className="px-5 py-2 border-2 rounded-3xl mt-3 hover:bg-[#f1f0e878]"
+                            className="px-5 py-2 border-2 rounded-3xl mt-3 hover:bg-[#f1f0e878] text-white"
                             onClick={form.handleSubmit(onsubmit)}
                         >
                             SEND
--- src/pages/Protfolio.tsx
+++ src/pages/Protfolio.tsx
@@ -7,12 +7,13 @@ import Connect from '../components/Connect'
 
 const Protfolio: React.FC = () => {
     return (
-        <div className="bg-cover bg-center bg-[url('/images/background.png')] bg-animate-bounce duration-700">
+        <div className="bg-cover bg-center bg-[url('/images/background.png')] overflow-hidden">
             <Navbar />
-            <HeroSection/>
-            <HoverDevCards/>
-            <Projects/>
-            <Connect/>
+            <HeroSection />
+            <HoverDevCards />
+            <Projects />
+            <Connect />
+
             <div className='h-24 px-4 py-6 text-center'>
                 <h2 className='text-2xl md:text-4xl font-bold text-[#AF1740] '>Keep coding and exploring</h2>
             </div>
